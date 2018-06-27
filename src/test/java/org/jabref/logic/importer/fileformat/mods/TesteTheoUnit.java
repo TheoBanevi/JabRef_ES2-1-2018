68d912acdae082dfce41d914318938b0a0e0c7b1
RobertaBrandao <beta.enghaw@gmail.com>
2018-06-27 18:09:44 -0300

    Novo Theo pre-implementado

---
+++ src/test/java/org/jabref/logic/importer/fileformat/mods/TesteTheoUnit.java
@@ -0,0 +1,14 @@
+package org.jabref.logic.importer.fileformat.mods;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TesteTheoUnit {
+
+	@Test
+	void test() {
+		fail("Theo was Not yet implemented");
+	}
+
+}
