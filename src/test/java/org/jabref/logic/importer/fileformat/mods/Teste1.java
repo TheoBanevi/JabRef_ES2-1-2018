8a6e9642f25584dcbed03f0d3df3c32fb5459830
unknown <beta.enghaw@gmail.com>
2018-06-27 17:34:19 -0300

    Teste da Jabiraca

---
+++ src/test/java/org/jabref/logic/importer/fileformat/mods/Teste1.java
@@ -0,0 +1,7 @@
+package org.jabref.logic.importer.fileformat.mods;
+
+public class Teste1 {
+    public static void main(String[] args) {
+               System.out.println("AGORA VAI (???)");
+         }
+}
