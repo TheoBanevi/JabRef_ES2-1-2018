664a4ff46d483239c9abcdef0e922787aed556ca
RobertaBrandao <beta.enghaw@gmail.com>
2018-06-27 18:06:11 -0300

    Novas classes de teste

---
+++ src/test/java/org/jabref/logic/importer/fileformat/mods/Teste2.java
@@ -0,0 +1,7 @@
+package org.jabref.logic.importer.fileformat.mods;
+
+public class Teste2 {
+	public static void main(String[] args) {
+        System.out.println("Eu só queria aparecer como contruibuidora");
+  }
+}
---
+++ src/test/java/org/jabref/logic/importer/fileformat/mods/Teste3.java
@@ -0,0 +1,7 @@
+package org.jabref.logic.importer.fileformat.mods;
+
+public class Teste3 {
+	public static void main(String[] args) {
+        System.out.println("MUDA BRASIL!!");
+  }
+}
